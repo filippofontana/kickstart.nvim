786b08dd8cf01f1e9bdfa97e6aa3b5f9887d8d6a
Filippo Fontana <filippofontana1998@gmail.com>
2024-07-02 12:48:11 +0200

    Add textual description to undotree command
    Signed-off-by: Filippo Fontana <filippofontana1998@gmail.com>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -97,7 +97,7 @@ return {
     dependencies = 'nvim-lua/plenary.nvim',
     config = true,
     keys = { -- load the plugin only when using it's keybinding:
-      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", { desc = 'Open undotree' } },
     },
   },
 }

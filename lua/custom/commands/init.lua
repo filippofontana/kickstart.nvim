e6c8112b188ff956cd18e474dca56e5db8a8342b
Filippo Fontana <filippofontana1998@gmail.com>
2024-08-12 16:28:38 +0200

    Add CopyAbsPath custom command
    Due to a wrongly copied function, this commit adds the real
    configuration for copying a relative path and also adds the function to
    copy the absolute path.
    
    Signed-off-by: Filippo Fontana <filippofontana1998@gmail.com>

--- lua/custom/commands/init.lua
+++ lua/custom/commands/init.lua
@@ -1,6 +1,12 @@
 -- Custom command to copy the relative path of the file stored in the
 -- current buffer
 vim.api.nvim_create_user_command('CopyRelPath', function()
+  local path = vim.fn.expand '%'
+  vim.fn.setreg('+', path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.api.nvim_create_user_command('CopyAbsPath', function()
   local path = vim.fn.expand '%:p'
   vim.fn.setreg('+', path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')

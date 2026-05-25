bfe76a70dfaefa4d4550cb5bf124ff2a6f14ebc6
Filippo Fontana <filippofontana1998@gmail.com>
2026-05-25 21:00:08 +0200

    init: include custom commands for copying file paths
    Signed-off-by: Filippo Fontana <filippofontana1998@gmail.com>

--- init.lua
+++ init.lua
@@ -973,5 +973,8 @@ do
   require 'custom.plugins'
 end
 
+-- Custom commands that are not related to kickstart-nvim which I personally use
+require 'custom.commands'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

9b87f4be05660d2bd9de18f282b5ceb010727f3d
Filippo Fontana <filippofontana1998@gmail.com>
2026-05-26 11:23:04 +0200

    init, mason: add custom command for installing additional md-format plugins
    All the collection of external plugins supported by md-format are not
    available through Mason, thus you have to install them manually through
    Mason's virtualenv or through a custom command like this.
    
    Signed-off-by: Filippo Fontana <filippofontana1998@gmail.com>

--- init.lua
+++ init.lua
@@ -981,6 +981,7 @@ end
 
 -- Custom commands that are not related to kickstart-nvim which I personally use
 require 'custom.commands'
+require 'custom.mason'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
